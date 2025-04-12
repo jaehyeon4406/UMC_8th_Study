38d7a32c01881ee8cebef7157fa62b65ec80df1c
jaehyeon4406 <jaehyeon4406@gmail.com>
2025-04-12 22:21:54 +0900

    Fix: finally added umc_spring as regular folder

---
+++ umc_spring/spring/src/main/java/umc/spring/study/domain/FoodCategory.java
@@ -0,0 +1,27 @@
+package umc.spring.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.study.domain.common.BaseEntity;
+import umc.spring.study.domain.mapping.MemberPrefer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @OneToMany(mappedBy = "foodCategory", cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
+}
---
+++ umc_spring/spring/src/main/java/umc/spring/study/domain/Mission.java
@@ -0,0 +1,41 @@
+package umc.spring.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.study.domain.Member;
+import umc.spring.study.domain.Store;
+import umc.spring.study.domain.common.BaseEntity;
+import umc.spring.study.domain.mapping.MemberMission;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer reward;
+
+    private LocalDate deadline;
+
+    private String missionSpec;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
